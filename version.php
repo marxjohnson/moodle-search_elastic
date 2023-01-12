09755d9c475b3be8ffad68b1eb433d2c9b3371fe
Dmitrii Metelkin <dmitriim@catalyst-au.net>
2023-01-13 10:52:59 +1100

    Bump version

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022120600;
-$plugin->release   = '4.1 Build (2022120600)'; // Build same as version.
+$plugin->version   = 2023011200;
+$plugin->release   = '4.1 Build (2023011200)'; // Build same as version.
 $plugin->requires  = 2016052304;
 $plugin->component = 'search_elastic';
 $plugin->maturity  = MATURITY_STABLE;
